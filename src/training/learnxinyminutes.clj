770b15d396e18e5ad5cba55b62e0f1ebc97f9b19
Izaak Khaliq <izaak@izaak.my>
2018-09-25 17:15:31 +0800

    learnx..

--- src/training/learnxinyminutes.clj
+++ src/training/learnxinyminutes.clj
@@ -35,7 +35,7 @@
 
 (def y 1)
 
-x
+y
 
 (def hello-world (fn [] "Hello World!"))
 
@@ -56,10 +56,150 @@ x
 
 (defn hello3 
   ([] "Hello World!")
-  ([name] "Hello " name))
+  ([name] (str "Hello " name)))
 
 (hello3)
 (hello3 "Master Izaak!")
+
+(defn count-args [& args]
+  (str "You passed " (count args) " args: " args))
+
+(count-args 3 2 1 :vd 12 :sdfs)
+
+(defn hello-count [name & args]
+  (str "Hello " name ", you passed " (count args) " extra args"))
+
+(hello-count "Master Izaak" 1 2 3 4)
+
+(class {:a 1 :b 2 :c 3})
+
+(class (hash-map :a 1 :b 2 :c 3))
+
+(class :a)
+
+(def stringmap {"a" 1, "b" 2, "c" 3})
+
+stringmap
+
+(def keymap {:a 1, :b 2, :c 3})
+
+keymap
+
+(stringmap "a")
+
+(keymap :b)
+
+(:c keymap)
+
+(def newkeymap (assoc keymap :d 4))
+
+newkeymap
+
+keymap
+
+(dissoc keymap :a :b)
+
+(class #{1 2 3})
+
+(set [1 2 3 1 2 3 1 2 3])
+
+(conj #{1 2 3 4} 5)
+
+(disj #{1 2 3} 1)
+
+(#{1 2 3} 1)
+(#{1 2 3} 4)
+
+(if false "a" "b")
+(if false "a")
+
+(let [a 1 b 2]
+  (> a b))
+
+(do
+  (print "Hello")
+  "World")
+
+(defn print-and-say-hello [name]
+  (print "Saying hello to" name)
+  (str "Hello " name))
+
+(print-and-say-hello "Izaak")
+
+(let [name "Urkel"]
+  (print "Saying hello to " name)
+  (str "Hello " name))
+
+(->
+  {:a 1 :b 2}
+  (assoc :c 3)
+  (dissoc :b))
+
+(->>
+  (range 10)
+  (map inc)
+  (filter odd?)
+  (into []))
+
+(as-> [1 2 3] input
+  (map inc input)
+  (nth input 2)
+  (conj [4 5 6] input [8 9 10]))
+
+(use 'clojure.set)
+
+(intersection #{1 2 3} #{2 3 4})
+(difference #{1 2 3} #{2 3 4})
+
+(use '[clojure.set :only [intersection]])
+
+(require 'clojure.string)
+
+(clojure.string/blank? "")
+
+(require '[clojure.string :as str])
+
+(str/replace "This is a test." #"[a-o]" str/upper-case)
+
+(ns test
+  (:require 
+    [clojure.string :as str]
+    [clojure.set :as set]))
+
+(import java.util.Date)
+
+(ns test
+  (:import java.util.Date
+          java.util.Calendar))
+
+(Date.)
+
+(. (Date.) getTime)
+(.getTime (Date.))
+
+(System/currentTimeMillis)
+
+(import java.util.Calendar)
+(doto (Calendar/getInstance)
+  (.set 2000 1 1 0 0 0)
+  .getTime)
+
+(def my-atom (atom {}))
+
+(swap! my-atom assoc :a 1)
+(swap! my-atom assoc :b 2)
+
+my-atom
+@my-atom
+
+(def counter (atom 0))
+(defn inc-counter []
+  (swap! counter inc))
+
+(inc-counter)
+
+@counter
+
 
 
 
